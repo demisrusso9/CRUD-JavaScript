26a12f88ea8ebad27dd1c8a685a9d56f39d56d62
Demis Russo <demisjunior@hotmail.com>
2020-09-30 21:37:11 -0300

    Added localStorage and dark-mode

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-let list = [];
+let list = JSON.parse(localStorage.getItem('favorites')) || [];
 
-document.querySelector('#add').addEventListener('click', createItem)
+document.querySelector('.input').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') createItem()
+    document.querySelector('.add').addEventListener('click', createItem)
+})
 
+document.querySelector('.local').addEventListener('click', showSavedData);
+document.querySelector('.delete').addEventListener('click', deleteSavedData);
+document.querySelector('.dark').addEventListener('click', darkMode);
+
+//CRUD
 function createItem() {
     let input = document.querySelector('.input');
+
+    if (!input.value.trim()) return;
 
     let obj = { id: Math.random() + 1, name: input.value }
     list.push(obj)
@@ -11,6 +21,7 @@ function createItem() {
     input.value = "";
     input.focus()
     updateAll()
+    saveLocalStorage()
 }
 
 function renameItem(id) {
@@ -22,14 +33,28 @@ function renameItem(id) {
 
 function updateAll() {
     document.querySelector('.list').innerHTML = ""
-    list.map(item => createElements(item))
-    console.log(list);
+    list.map(item => createElements(item))    
 }
 
 function deleteItem(id) {
     let array = list.filter(item => item.id != id)
     list = array;
     updateAll()
+}
+
+//Local Storage
+function saveLocalStorage() {
+    localStorage.setItem('favorites', JSON.stringify(list))
+}
+
+function showSavedData() {
+    list.map(item => createElements(item))
+}
+
+function deleteSavedData() {
+    updateAll()
+    list.filter(item => deleteItem(item.id))
+    localStorage.clear();
 }
 
 // Utils Functions
@@ -46,7 +71,7 @@ function createElements(item) {
 
     renameBtn.onclick = () => renameItem(item.id)
     deleteBtn.onclick = () => deleteItem(item.id)
-
+    
     p.textContent = item.name;
     p.append(renameBtn, deleteBtn)
     document.querySelector('.list').appendChild(p)
@@ -64,7 +89,7 @@ function createRenameElement(item) {
     btn.textContent = 'Salvar';
 
     btn.onclick = () => saveUpdate(item.id, input.value);
-    
+
     div.append(input, btn)
     document.querySelector('.list').append(div)
 }
@@ -75,4 +100,10 @@ function saveUpdate(id, value) {
     })
 
     updateAll()
+}
+
+function darkMode() {
+    document.querySelector('.page-header').classList.toggle('dark-mode-header')
+    document.querySelector('body').classList.toggle('dark-mode-body')    
+    document.querySelector('.list').classList.toggle('dark-mode-list')    
 }
